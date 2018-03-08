ba85eaa3915695d613eae893a16cb7ba784fc241
BrandonVolesky <37189855+BrandonVolesky@users.noreply.github.com>
2018-03-08 12:42:01 -0600

    Initialized Source.cpp & Evaluator.h

---
+++ Evaluator.h
@@ -0,0 +1,21 @@
+#pragma once
+// Evaluator Class
+class Evaluator
+{
+public:
+	Evaluator();
+	~Evaluator();
+
+private:
+
+};
+
+// Default Contructor
+Evaluator::Evaluator()
+{
+}
+
+// Destructor
+Evaluator::~Evaluator()
+{
+}
---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include "Evaluator.h" // The Evaluator Class is in this file
+
+using namespace std;
+
+
+// We call this in the main function
+void eval() {
+
+// Code goes here
+}
+
+
+
+
+
+// Main
+int main() {
+
+	Evaluator eval;
+	int result = eval.eval("!!!3+2");
+
+	return 0;
+
+}
