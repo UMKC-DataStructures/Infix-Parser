adfe0d7f40fa9e8471dade10ea252fe950786c28
Brandon Volesky <37189855+BrandonVolesky@users.noreply.github.com>
2018-03-09 22:21:02 -0600

    Test

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void main() {
-	cout << "Why is Visual Studio such a HEADACHE" << endl;
+	cout << "Why is VISUAL STUDIO such a HEADACHE" << endl;
 	cout << "This is a test." << endl;
 	//This is another commit test
 }
