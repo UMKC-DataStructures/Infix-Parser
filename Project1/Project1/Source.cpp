71f009581befa8c146fc40c800721f1f8e58f94c
Brandon Volesky <37189855+BrandonVolesky@users.noreply.github.com>
2018-03-09 22:18:09 -0600

    Collision Test

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void main() {
-	cout << "Why is GitHub such a HEADACHE" << endl;
+	cout << "Why is Visual Studio such a HEADACHE" << endl;
 	cout << "This is a test." << endl;
 	//This is another commit test
 }
