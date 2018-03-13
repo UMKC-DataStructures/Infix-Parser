fcb461fdcdc180ccb3c02c6e5a66cd0a0e1a6db5
korlandril <taa522@mail.umkc.edu>
2018-03-13 17:29:26 -0500

    Brandon quit editing the cpp you made open the solution
    Brandon quit editing the cpp you made

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,4 +5,5 @@ void main() {
 	cout << "Why is VISUAL STUDIO such a HEADACHE" << endl;
 	cout << "This is a test." << endl;
 	//This is another commit test
+	//Brandon you need to open the solution in the repo not the Source.cpp you made
 }
